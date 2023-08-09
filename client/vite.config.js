a02f9fd7cfec540ce8e065b8d17804a067d6b29a
Isaque Medeiros <isaquemedeirosxx@gmail.com>
2023-08-09 17:16:40 -0300

    Commit Inicial

---
+++ client/vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  base: "./",
+  server: {
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      "./runtimeConfig": "./runtimeConfig.browser",
+    },
+  },
+  build: {
+    chunkSizeWarningLimit: 1600,
+  },
+  define: {
+    global: "globalThis",
+  },
+});
