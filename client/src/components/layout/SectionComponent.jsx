54d222eead6fb6b49a0c5be002c12251dcc8065a
Víctor Souza <victorbatistasouza2002@gmail.com>
2023-08-09 22:21:59 -0300

    who we are added

--- client/src/components/layout/SectionComponent.jsx
+++ client/src/components/layout/SectionComponent.jsx
@@ -1,11 +1,12 @@
-function SectionComponent({sectionBg, children}) {
+function SectionComponent({ sectionBg, children, haveHeader }) {
   return (
     <section className={sectionBg}>
-      <div 
-      className="
-        w-[90%] max-w-[1400px] min-h-[100dvh] mx-auto flex flex-col gap-8 items-center justify-center 
-        md:w-[80%] md:min-h-[75dvh]
-      "
+      <div
+        className={`
+          w-[90%] max-w-[1400px] min-h-[100dvh] mx-auto flex flex-col gap-8 
+          items-center justify-center overflow-hidden ${haveHeader === true ? "py-24" : ""}
+          md:w-[80%] md:min-h-[75dvh]
+        `}
       >
         {children}
       </div>
